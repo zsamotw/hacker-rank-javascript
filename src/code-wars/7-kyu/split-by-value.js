dd1437b08cd6ce0437dc6a57253e7dccf6045d62
zsamotw <donkemot@interia.pl>
2020-09-12 19:21:46 +0200

    Use spread operator for concatenation.

--- src/code-wars/7-kyu/split-by-value.js
+++ src/code-wars/7-kyu/split-by-value.js
@@ -1,5 +1,5 @@
 function splitByValue(k, elements) {
-  return elements.filter(el => el < k).concat(elements.filter(el => el >= k))
+  return [...elements.filter(el => el < k), ...elements.filter(el => el >= k)]
 }
 
 module.exports = splitByValue
