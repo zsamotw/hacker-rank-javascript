458a3d22081a656846954b0ccf815bf91ad464e5
zsamotw <donkemot@interia.pl>
2021-04-02 19:08:11 +0200

    Sum of multiples fixes

--- src/code-wars/7-kyu/sum-of-multiples.js
+++ src/code-wars/7-kyu/sum-of-multiples.js
@@ -2,7 +2,7 @@ function sumOfMultiplesOf3Or5(num) {
   if (num <= 0) {
     return 0
   } else {
-    const nums = [...Array(num).keys()]
+    const nums = [...Array(num).keys(), num]
     return nums.reduce((result, num) => {
       if (num % 3 === 0 || num % 5 === 0) return result + num
       else return result
--- src/tests/code-wars/sum-of-multiples.test.js
+++ src/tests/code-wars/sum-of-multiples.test.js
@@ -1,8 +1,12 @@
-const sumOfMultiplesOf3Or5 = require('../../code-wars/6-kyu/sum-of-multiples')
+const sumOfMultiplesOf3Or5 = require('../../code-wars/7-kyu/sum-of-multiples')
 
 describe('Sum of multiple of 3 or 5', () => {
   it('Sum should be equal', () => {
-    expect(sumOfMultiplesOf3Or5(0)).toEqual(0)
-    expect(sumOfMultiplesOf3Or5(10)).toEqual(23)
+    expect(sumOfMultiplesOf3Or5(5)).toEqual(8)
+    expect(sumOfMultiplesOf3Or5(10)).toEqual(33)
+    expect(sumOfMultiplesOf3Or5(100)).toEqual(2418)
+    expect(sumOfMultiplesOf3Or5(1000)).toEqual(234168)
+    expect(sumOfMultiplesOf3Or5(10000)).toEqual(23341668)
+    expect(sumOfMultiplesOf3Or5(100000)).toEqual(2333416668)
   })
 })
