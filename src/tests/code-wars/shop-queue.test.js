3604c2d7370e0de0e5e509c51b438215bbe3cb56
zsamotw <donkemot@interia.pl>
2021-04-02 19:46:25 +0200

    Add more shop queue tests

--- src/tests/code-wars/shop-queue.test.js
+++ src/tests/code-wars/shop-queue.test.js
@@ -6,5 +6,11 @@ describe('Queue shop time', () => {
     expect(queueTime([1, 2, 3, 4], 1)).toEqual(10)
     expect(queueTime([2, 2, 3, 3, 4, 4], 2)).toEqual(9)
     expect(queueTime([1, 2, 3, 4, 5], 100)).toEqual(5)
+    expect(
+      queueTime(
+        [43, 24, 49, 8, 8, 10, 29, 48, 5, 20, 4, 34, 22, 39, 3, 30, 24],
+        3
+      )
+    ).toEqual(136)
   })
 })
