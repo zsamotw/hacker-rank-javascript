17fcccf48ac71a4d9ae37111be532cbdb1a0e2d9
zsamotw <donkemot@interia.pl>
2021-04-02 19:46:01 +0200

    Implement flatmap function

--- src/code-wars/6-kyu/simple-fun302.js
+++ src/code-wars/6-kyu/simple-fun302.js
@@ -1,18 +1,26 @@
 function maxSum(num) {
+  const flatMap = (f, xs) => xs.reduce((r, x) => r.concat(f(x)), [])
+
   const range = [...Array(num).keys(), num]
-  return range
-    .flatMap(x =>
-      range.flatMap(y =>
-        range.map(z => {
-          if ((x + y) % 2 === 0 && (y + z) % 3 === 0 && (x + y + z) % 5 == 0) {
-            return x + y + z
-          } else {
-            return 0
-          }
-        })
-      )
-    )
-    .reduce((max, el) => Math.max(max, el))
+  return flatMap(
+    x =>
+      flatMap(
+        y =>
+          range.map(z => {
+            if (
+              (x + y) % 2 === 0 &&
+              (y + z) % 3 === 0 &&
+              (x + y + z) % 5 == 0
+            ) {
+              return x + y + z
+            } else {
+              return 0
+            }
+          }),
+        range
+      ),
+    range
+  ).reduce((max, el) => Math.max(max, el))
 }
 
 module.exports = maxSum
